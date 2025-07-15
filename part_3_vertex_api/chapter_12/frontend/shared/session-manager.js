ffffd5570335756893e950de528fca0ff5d4cd13
oglcncvzc <cevizciogulcan@gmail.com>
2025-07-15 22:38:19 +0300

    Son hali cache

--- part_3_vertex_api/chapter_12/frontend/shared/session-manager.js
+++ part_3_vertex_api/chapter_12/frontend/shared/session-manager.js
@@ -2,7 +2,7 @@
 // Kullanıcıya özel session yönetimi için yardımcı fonksiyonlar
 
 export class SessionManager {
-  constructor(proxyBaseUrl = 'https://vertex-proxy-service-638345404110.us-central1.run.app') {
+  constructor(proxyBaseUrl = 'http://localhost:8080') {
     this.proxyBaseUrl = proxyBaseUrl;
     this.currentSessionId = null;
     this.currentUserId = null;
